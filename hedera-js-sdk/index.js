e822cccdeef8e8c5d19a745d3ce504e8de013059
Gurjinder Singh <rockygsingh@icloud.com>
2022-04-18 01:33:43 -0400

    set up the connection using .env file

--- hedera-js-sdk/index.js
+++ hedera-js-sdk/index.js
@@ -1,42 +1,39 @@
+require('dotenv').config();
+
 const { Client, AccountBalanceQuery } = require("@hashgraph/sdk");
 
-require("dotenv").config();
-
 async function main() {
-
+  
+    try {
 
     // Grab your Hedera account ID and Key
-    const myAccountId = process.env.MY_ACCOUNT_ID;
-    const myPrivateKey = process.env.MY_PRIVATE_KEY;
+    const myAccountId = process.env.ACCOUNTID;
+    const myPrivateKey = process.env.PRIVATEKEY;
+
 
 
     // throw an error if you couldn't get the id
-    if (myAccountId == null || myPrivateKey == null ) {
-        throw new Error("Environment variables must be present");
+    if (myAccountId == null || myPrivateKey == null) {
+      throw new Error("Environment variables must be present");
     }
 
 
-    // connect to the hedera network
-    const client = new Client({
-        network: { "0.testnet.hedera.com:50211": "0.0.3" },
-        operator: {
-            account: myAccountId,
-            privateKey: myPrivateKey
-        }
-    });
 
+    // connect to the hedera network
+    const client = Client.forTestnet();
+
+    client.setOperator(myAccountId, myPrivateKey);
 
     // Now the connection to the hedera network is established
 
-
-    // get your balance 
+    // get your balance
     const balance = await new AccountBalanceQuery()
-        .setAccountId(myAccountId)
-        .execute(client)
-
-
-    console.log(`${myAccountId} balance =  ${balance.value()}`);
+      .setAccountId(myAccountId)
+      .execute(client);
+    
+  } catch (e) {
+    console.log(e);
+  }
 }
-
 
 main();
